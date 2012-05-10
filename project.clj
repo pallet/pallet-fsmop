07d442efbc7633fc70fb4d36cb5b8ba158668d34
Hugo Duncan <hugo@hugoduncan.org>
2012-05-10 10:50:25 -0400

    Update to clojure 1.2.1 as minimum clojure dependency

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/palletops/pallet-fsmop"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/tools.logging "0.2.3"]
                  [pallet-fsm "0.1.0"]
                  [pallet-map-merge "0.1.0"]
