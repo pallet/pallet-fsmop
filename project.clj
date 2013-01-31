acecbe7f1f898d3789d8580d8219cc8392039442
Hugo Duncan <hugo@hugoduncan.org>
2013-01-31 11:48:45 -0500

    Update to pallet-fsm 0.2.0

--- project.clj
+++ project.clj
@@ -5,6 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.logging "0.2.3"]
-                 [pallet-fsm "0.2.0-SNAPSHOT"]
+                 [pallet-fsm "0.2.0"]
                  [pallet-map-merge "0.1.0"]
                  [pallet-thread "0.1.0"]])
