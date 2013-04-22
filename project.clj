eed80ae5d937fb43ac0fdcc3fe403c5be515a35a
Hugo Duncan <hugo@hugoduncan.org>
2013-04-22 14:35:27 -0400

    Fix version to include SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pallet-fsmop "0.2.8"
+(defproject pallet-fsmop "0.2.8-SNAPSHOT"
   :description "FSM composition"
   :url "https://github.com/pallet/pallet-fsmop"
   :license {:name "Eclipse Public License"
