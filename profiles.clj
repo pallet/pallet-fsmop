b07d74357b389ec066175a382cd98e61347d101c
Hugo Duncan <hugo@hugoduncan.org>
2013-05-23 12:35:59 -0400

    Updated project.clj, release notes and readme for 0.3.0

--- profiles.clj
+++ profiles.clj
@@ -2,12 +2,12 @@
        :warn-on-reflection true}
  :clojure-1.5 {:dependencies [[org.clojure/clojure "1.5.0"]]}
  :codox {:codox {:writer codox-md.writer/write-docs
-                 :output-dir "doc/0.2"}
+                 :output-dir "doc/0.3"}
          :dependencies [[codox-md "0.1.0"]
                         [codox/codox.core "0.6.1"]]
          :pedantic :warn}
  :marginalia {:pedantic :warn
-              :dir "doc/0.2/annotated"}
+              :dir "doc/0.3/annotated"}
  :release
  {:plugins [[lein-set-version "0.2.1"]]
   :set-version
