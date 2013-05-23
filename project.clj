8b928c5a65ffd1326571fa8bee514e6d3b77d722
Hugo Duncan <hugo@hugoduncan.org>
2013-05-23 09:19:28 -0400

    Update to tools.logging 0.2.6
    Removes compilation warnings.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/tools.logging "0.2.3"]
+                 [org.clojure/tools.logging "0.2.6"]
                  [pallet-fsm "0.2.0"]
                  [pallet-map-merge "0.1.0"]
                  [pallet-thread "0.1.0"]])
